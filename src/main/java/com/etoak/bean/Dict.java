e11c3853be92a4b0e18c5b37c993bf3dc1f8d7ce
jiayu <1847438558@qq.com>
2020-04-20 23:47:33 +0800

    地区查询接口开发完毕

--- src/main/java/com/etoak/bean/Dict.java
+++ src/main/java/com/etoak/bean/Dict.java
@@ -1,15 +1,12 @@
 package com.etoak.bean;
 
-/**
- * Created by 18474 on 2020/4/20.
- */
 import lombok.Data;
 
 @Data
 public class Dict {
-
     private Integer id;
     private String groupId;
     private String name;
     private String value;
-    private Integer sort;}
+    private Integer sort;
+}
--- src/main/java/com/etoak/bean/User.java
+++ src/main/java/com/etoak/bean/User.java
@@ -1,12 +1,8 @@
 package com.etoak.bean;
 
-
 import lombok.Data;
 
 @Data
-/**
- * Created by 18474 on 2020/4/20.
- */
 public class User {
     private Integer id;
     private String name;
@@ -14,5 +10,4 @@ public class User {
     private String gender;
     private Integer age;
     private String birthday;
-
 }
